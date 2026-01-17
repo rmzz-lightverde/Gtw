eb784f3ed56439117057b44221bfe0a716ef660f
rmzz-lightverde <inaarama60@gmail.com>
2026-01-17 15:19:05 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {<
+  TOKEN_BOT: ["8399905242:AAH1WxalIoAN3Xe-j3Jw4m9JBWvHhBQCCgw"],
+};
+/*/━━━━━━━━━━━━━━━━━━━━━⬡═—⊱  
+    👑 𝗫 𝗦𝗧𝗥𝗢𝗠 𝗜𝗡𝗙𝗜𝗡𝗜𝗧𝗬 👑
+━━━━━━━━━━━━━━━━━━━━━━━⬡═—⊱    
+
+🚀 owner  : ×͜×
+📢 Info       : RAMZYNOTDEV
+🛠️ Version    : V1.7
+
+⚠️ 警告! ⚠️  
+📌 此机器人使用数据库。  
+📌 如果您的令牌 未在数据库中注册，  
+📌 机器人将 无法运行！  
+📌 请确保您的令牌已注册后再使用本机器人。  
+
+⚠️ PERINGATAN! ⚠️  
+📌 Bot ini menggunakan database.  
+📌 Jika token Anda tidak terdaftar di database,  
+📌 bot tidak akan berjalan!  
+📌 Pastikan token Anda sudah terdaftar sebelum menggunakan bot ini.  
+
+━━━━━━━━━━━━━━━━━━━━━━/*/
